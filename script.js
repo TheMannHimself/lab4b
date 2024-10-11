9462c45e96221086634ef7cea4d9d656934d7b6d
TheMannHimself <156624443+TheMannHimself@users.noreply.github.com>
2024-10-10 23:07:16 -0400

    Update script.js
    moved the preventDefault event to allow the form to be summited

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const form = document.getElementById('userForm');
 
   form.addEventListener('submit', function(event) {
-    event.preventDefault(); 
+    
 
     const email = document.getElementById('email').value;
     const phone = document.getElementById('phone').value;
@@ -19,6 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
     if (email && phone && dob) {
       alert(`Form Submitted! \nEmail: ${email}\nPhone: ${phone}\nDate of Birth: ${dob}`);
     } else {
+      event.preventDefault(); 
       alert('Please fill out all fields.');
     }
   });
