9cbd469683e39bc063660bf547ba35b43ee1797c
TheMannHimself <156624443+TheMannHimself@users.noreply.github.com>
2024-10-11 21:07:52 -0400

    Update script.js
    got rid of the dob >18 checker, fixed variable names

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
   form.addEventListener('submit', function(event) {
     
+    const name = document.getElementById('fullName').value;
     const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
     const dob = document.getElementById('dob').value;
   
     if (email && phone && dob) {
-      alert(`Form Submitted! \nEmail: ${email}\nPhone: ${phone}\nDate of Birth: ${dob}`);
+      alert(`Form Submitted! \nName: ${name}\nEmail: ${email}\nDate of Birth: ${dob}`);
     } else {
       event.preventDefault(); 
       alert('Please fill out all fields.');
